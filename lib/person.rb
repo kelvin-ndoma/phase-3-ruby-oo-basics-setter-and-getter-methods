6f8ea9669bf4df83d2ad5774e898a6a75cb71eeb
kevin.mutua@student.moringaschool.com <kevin.mutua@student.moringaschool.com>
2023-02-10 11:39:27 +0300

    completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,27 @@
 class Person
+    #setter method
+    def name=(name)
+        @name= name
+    end
+
+    #getter method
+    def name 
+        @name
+    end
+
+    def job=(job)
+        @job=job
+    end
+
+    def job
+        @job
+    end
 
 end
+
+kanye = Person.new
+
+puts kanye.name = "Kanye"
+
+beyonce=Person.new
+ puts beyonce.job = "Singer"
